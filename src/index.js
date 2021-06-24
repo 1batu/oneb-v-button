e4b61b6ee6e096ef07b4ebd7d77902aea059a3c1
batuhanhaymana <32592602+batuhanhaymana@users.noreply.github.com>
2021-06-24 23:33:53 +0300

    button testing

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import onebvButton from "./onebvbutton.vue";
+
+function install(Vue) {
+  if (install.installed) return;
+  install.installed = true;
+  Vue.component("v-onebvbutton", onebvButton);
+}
+const plugin = {
+  install,
+};
+let GlobalVue = null;
+if (typeof window !== "undefined") {
+  GlobalVue = window.Vue;
+} else if (typeof global !== "undefined") {
+  GlobalVue = global.vue;
+}
+if (GlobalVue) {
+  GlobalVue.use(plugin);
+}
+
+onebvButton.install = install;
+
+export default onebvButton;
